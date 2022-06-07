c37356b30581581fccd3f10b8606a0122791b13d
AishwaryaPalanivel <47705869+AishwaryaPalanivel@users.noreply.github.com>
2022-06-07 17:16:56 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,26 @@
+package week6.day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+@Test
+public class CreateLead extends ProjectSpecificMethods{
+	
+	public  void createLead()
+	{
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+	
+}
+}
